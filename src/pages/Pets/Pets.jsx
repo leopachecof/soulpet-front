614d1949231112b78db1150023ecd5700cf8bae3
leopachecof <l.nikiti@gmail.com>
2023-04-28 23:37:27 -0300

    [FE-2] Integrar a remoção de Pets#5

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,22 +1,58 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
+import { Loader } from "../../components/Loader/Loader";
+import { toast } from "react-hot-toast";
 
 export function Pets() {
     const [pets, setPets] = useState(null);
     const { id } = useParams();
 
+    const [show, setShow] = useState(false);
+    const [idPet, setIdPet] = useState(null);
+  
+    const handleClose = () => {
+        setIdPet(null);
+        setShow(false)
+    };
+    const handleShow = (id) => {
+        setIdPet(id);
+        setShow(true)
+    };
+
     useEffect(() => {
-        axios
-            .get(`http://localhost:3001/pets`)
-            .then((response) => {
-                setPets(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        initializeTable();
+    }, []);
+
+
+    useEffect(() => {
+        initializeTable();
     }, [id]);
+
+    function initializeTable() {
+        axios
+        .get(`http://localhost:3001/pets`)
+        .then((response) => {
+            setPets(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+    function onDelete() {
+        axios.delete(`http://localhost:3001/pets/${idPet}`)
+            .then(response => {
+                toast.success(response.data.message, { position: "bottom-right", duration: 2000 });
+                initializeTable();
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.response.data.message, { position: "bottom-right", duration: 2000 });
+            });
+        handleClose();
+    }
 
     return (
         <div className="clientes container">
@@ -26,7 +62,10 @@ export function Pets() {
                     <i className="bi bi-plus-lg me-2"></i> Pet
                 </Button>
             </div>
-            
+            {
+                pets === null ?
+                    <Loader />
+                    :
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -57,7 +96,7 @@ export function Pets() {
                                         >
                                             <i className="bi bi-pencil-fill"></i>
                                         </Button>
-                                        <Button>
+                                        <Button onClick={() => handleShow(pet.id)}>
                                            <i className="bi bi-trash-fill"></i>
                                         </Button>
                                     </td>
@@ -71,6 +110,21 @@ export function Pets() {
                     )}
                 </tbody>
             </Table>
+            }
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmação</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Tem certeza que deseja excluir o cliente?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={onDelete}>
+                        Excluir
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
